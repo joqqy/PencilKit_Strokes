7b96978e035cc31ddd8657e57e5bb9d49ce06f81
Pierre Hanna <realbrush@gmail.com>
2022-03-23 20:48:50 +0100

    m

--- PencilKit_Strokes/ViewController.swift
+++ PencilKit_Strokes/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import PencilKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKCanvasViewDelegate {
+    
+    var canvasView: PKCanvasView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setUpPencilKit()
     }
+    
+    func setUpPencilKit() {
+        
+        self.canvasView = PKCanvasView(frame: self.view.bounds)
+        guard let canvasView = canvasView else { fatalError() }
+        canvasView.delegate = self
+        canvasView.becomeFirstResponder()
+        
+        view.addSubview(canvasView)
+    }
+    
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
 
-
+        let nextStrokeIndex: Int = canvasView.drawing.strokes.count
+        print("nextStrokeIndex: \(nextStrokeIndex)")
+        
+    }
+    
+    func canvasViewDidFinishRendering(_ canvasView: PKCanvasView) {
+    }
+    
+    func canvasViewDidEndUsingTool(_ canvasView: PKCanvasView) {
+        
+        canvasView.drawing = PKDrawing()
+        canvasView.drawing.strokes.removeAll()
+    }
 }
 
